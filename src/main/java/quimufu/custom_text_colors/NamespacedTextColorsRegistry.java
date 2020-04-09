7c7c205f35d7b8b00ca99aea3d170e9ad17883a4
Johannes Michael Andreas Merl <johannes.merl@student.uni-augsburg.de>
2020-04-09 02:59:54 +0200

    fix short flickering when re-enabling random coloring or seeing no text for some time

--- src/main/java/quimufu/custom_text_colors/NamespacedTextColorsRegistry.java
+++ src/main/java/quimufu/custom_text_colors/NamespacedTextColorsRegistry.java
@@ -5,7 +5,9 @@ import com.google.common.collect.Maps;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import net.minecraft.util.Util;
 import org.apache.logging.log4j.Level;
-import quimufu.custom_text_colors.jankson.*;
+import quimufu.custom_text_colors.jankson.JsonGrammar;
+import quimufu.custom_text_colors.jankson.JsonObject;
+import quimufu.custom_text_colors.jankson.JsonPrimitive;
 
 import java.util.Map;
 import java.util.Random;
@@ -23,8 +25,8 @@ public class NamespacedTextColorsRegistry {
     protected final CustomTextColorsConfig conf = AutoConfig.getConfigHolder(CustomTextColorsConfig.class).getConfig();
     protected final Random random = new Random();
     protected ChangingColor eyesoreChangingColor = new ChangingColorEyesore();
-    private Map<String,Integer> currentPRColors = Maps.newHashMap();
-    private Map<String,Integer> nextPRColors = Maps.newHashMap();
+    private Map<String, Integer> currentPRColors = Maps.newHashMap();
+    private Map<String, Integer> nextPRColors = Maps.newHashMap();
     private long time = -1;
 
     public NamespacedTextColorsRegistry(String ns, Map<String, Integer> cm, Map<String, ChangingColor> ccm, Default aDefault) {
@@ -56,34 +58,37 @@ public class NamespacedTextColorsRegistry {
     }
 
 
-
     public int getColor(String s, int color) {
         s = normalize(s);
         if (conf.eyesoreRandom) {
             return ((random.nextInt() | random.nextInt()) & 0xFFFFFF) | (color & 0xFF000000);
         }
-        if(conf.pulsatingRandom) {
-            long now = Util.getMeasuringTimeMs();            
-            if(time == -1){
+        if (conf.pulsatingRandom) {
+            long now = Util.getMeasuringTimeMs();
+            if (time == -1) {
                 time = now;
             }
-            int timeDelta = (int) (now-time);
-            if(timeDelta >= conf.pulseSpeedMs || timeDelta < 0){
+            int timeDelta = (int) (now - time);
+            if (timeDelta >= conf.pulseSpeedMs || timeDelta < 0) {
                 time = now - (timeDelta - conf.pulseSpeedMs);
-                timeDelta = (timeDelta - conf.pulseSpeedMs);
+                if (timeDelta > 2 * conf.pulseSpeedMs) {
+                    timeDelta = 0;
+                } else {
+                    timeDelta = (timeDelta - conf.pulseSpeedMs);
+                }
                 Map<String, Integer> tmp = currentPRColors;
                 currentPRColors = nextPRColors;
                 nextPRColors = tmp;
                 nextPRColors.clear();
             }
-            if(!currentPRColors.containsKey(s)){
+            if (!currentPRColors.containsKey(s)) {
                 currentPRColors.put(s, ((random.nextInt() | random.nextInt()) & 0xFFFFFF));
             }
-            if(!nextPRColors.containsKey(s)){
+            if (!nextPRColors.containsKey(s)) {
                 nextPRColors.put(s, ((random.nextInt() | random.nextInt()) & 0xFFFFFF));
             }
-            return mix(currentPRColors.get(s), nextPRColors.get(s), ((float)timeDelta)/((float)conf.pulseSpeedMs) ) | (color & 0xFF000000);
-            
+            return mix(currentPRColors.get(s), nextPRColors.get(s), ((float) timeDelta) / ((float) conf.pulseSpeedMs)) | (color & 0xFF000000);
+
         }
         if (colors.get(s) == null) {
             CustomTextColors.log(Level.ERROR, "unset color: " + s);
@@ -112,14 +117,14 @@ public class NamespacedTextColorsRegistry {
     }
 
     private int mix(int c1, int c2, float ratio) {
-        int r1,g1,b1,r2,g2,b2;
-        r1 = (int)((c1 & 0xFF0000)*(1.-ratio))& 0xFF0000;
-        g1 = (int)((c1 & 0xFF00)*(1.-ratio))& 0xFF00;
-        b1 = (int)((c1 & 0xFF)*(1.-ratio))& 0xFF;
-        r2 = (int)((c2 & 0xFF0000)*(ratio))& 0xFF0000;
-        g2 = (int)((c2 & 0xFF00)*(ratio))& 0xFF00;
-        b2 = (int)((c2 & 0xFF)*(ratio))& 0xFF;
-        return r1+g1+b1+r2+g2+b2;
+        int r1, g1, b1, r2, g2, b2;
+        r1 = (int) ((c1 & 0xFF0000) * (1. - ratio)) & 0xFF0000;
+        g1 = (int) ((c1 & 0xFF00) * (1. - ratio)) & 0xFF00;
+        b1 = (int) ((c1 & 0xFF) * (1. - ratio)) & 0xFF;
+        r2 = (int) ((c2 & 0xFF0000) * (ratio)) & 0xFF0000;
+        g2 = (int) ((c2 & 0xFF00) * (ratio)) & 0xFF00;
+        b2 = (int) ((c2 & 0xFF) * (ratio)) & 0xFF;
+        return r1 + g1 + b1 + r2 + g2 + b2;
     }
 
     public void reset(Map<String, Integer> cm, Map<String, ChangingColor> ccm, Default aDefault) {
