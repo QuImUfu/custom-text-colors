cce6b13afc20292e331e053237fa181562352da7
QuImUfu <vo_gel_frei@yahoo.de>
2020-04-10 21:44:47 +0200

    add ItemStack count color

---
+++ src/main/java/quimufu/custom_text_colors/mixin/gui_window/ItemRendererDrawTextMixin.java
@@ -0,0 +1,35 @@
+package quimufu.custom_text_colors.mixin.gui_window;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.util.math.Matrix4f;
+import org.apache.logging.log4j.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import quimufu.custom_text_colors.CustomTextColors;
+import quimufu.custom_text_colors.NamespacedTextColorsRegistry;
+import quimufu.custom_text_colors.TextColorsResourceManager;
+
+
+@Mixin(ItemRenderer.class)
+public class ItemRendererDrawTextMixin {
+    private final NamespacedTextColorsRegistry tcm = TextColorsResourceManager.getInstance().getNamespacedTextColorsRegistry(CustomTextColors.MOD_NAME);
+
+    @Redirect(method = "renderGuiItemOverlay(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/font/TextRenderer;draw(Ljava/lang/String;FFIZLnet/minecraft/client/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZII)I"))
+    private int drawCenteredStringChangeColor(TextRenderer textRenderer, String text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider vertexConsumerProvider, boolean seeThrough, int backgroundColor, int light) {
+        if (color == 16777215) {
+            int colorN = tcm.getColor("ItemStack.textColor.amount", color);
+            return textRenderer.draw(text, x, y, colorN, shadow, matrix, vertexConsumerProvider, seeThrough, backgroundColor, light);
+        } else {
+            CustomTextColors.log(Level.ERROR, "unknown value: " + color);
+            CustomTextColors.log(Level.ERROR, "falling back to default. " + this.getClass().getSimpleName() + " !report this to CustomTextColors!");
+        }
+        return textRenderer.draw(text, x, y, color, shadow, matrix, vertexConsumerProvider, seeThrough, backgroundColor, light);
+    }
+
+}
+
